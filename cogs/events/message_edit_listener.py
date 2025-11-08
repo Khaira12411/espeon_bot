82a6a9d6fbf7e76583704aa7220e1fba3d677da2
Kyra Paraiso <24mashpotato@gmail.com>
2025-11-08 14:16:10 +0800

    fixed on message edit error

--- cogs/events/message_edit_listener.py
+++ cogs/events/message_edit_listener.py
@@ -50,5 +50,5 @@ class MessageEditListener(commands.Cog):
                 )
 
 
-def setup(bot: commands.Bot):
-    bot.add_cog(MessageEditListener(bot))
+async def setup(bot: commands.Bot):
+    await bot.add_cog(MessageEditListener(bot))
